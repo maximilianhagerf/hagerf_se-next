c8d78ad2d1ecdfac42b9777140cb91572b1ec5d0
Maximilian Hagerf <maximilian.hagerf@gmail.com>
2021-08-13 21:10:25 -0400

    Fixed console error, added a 'a' tag

--- components/SocialLink.js
+++ components/SocialLink.js
@@ -1,17 +1,15 @@
+import React from "react";
 import Link from "next/link";
 import Image from "next/image";
 import styles from "../styles/modules/SocialLink.module.css";
 
-const SocialLink = ({ key, href, alt, icon }) => {
+const SocialLink = ({ socialKey, href, alt, icon }) => {
   return (
     <div className={styles.SocialLink}>
-      <Link
-        key={key}
-        href={href}
-        // passHref={true}
-        target="_blank"
-      >
-        <Image src={icon} alt={alt} className={styles.Icon} />
+      <Link key={socialKey} href={href} passHref={true} target="_blank">
+        <a>
+          <Image src={icon} alt={alt} className={styles.Icon} />
+        </a>
       </Link>
     </div>
   );

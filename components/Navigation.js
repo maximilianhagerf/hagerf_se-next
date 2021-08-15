77dcdde2c92bad9cd7e5584d0853bc62b281a62d
Maximilian Hagerf <maximilian.hagerf@gmail.com>
2021-08-14 20:01:03 -0400

    Navigation animation

--- components/Navigation.js
+++ components/Navigation.js
@@ -3,39 +3,69 @@ import { useRouter } from "next/dist/client/router";
 import { isActiveLink } from "../lib/utils";
 import Link, { LinkProps } from "next/link";
 import styles from "../styles/modules/Navigation.module.css";
+// import { variants } from "../tailwind.config";
 
 const links = [
   {
+    id: 0,
     name: "HOME",
     href: "/",
   },
   {
+    id: 1,
     name: "ABOUT",
     href: "/about",
   },
 ];
+
+const variants = {
+  hidden: {
+    y: -100,
+  },
+  visible: (custom) => ({
+    y: 0,
+    transition: {
+      delay: custom * 0.2,
+      duration: 0.5,
+    },
+  }),
+};
 
 function NavItem(props) {
   const isActive = isActiveLink(props.href, props.pathname);
 
   if (isActive) {
     return (
-      <Link key={props.name} href={props.href} scroll={false}>
-        <a className={`${styles.Link} ${styles.IsActive}`}>
-          <motion.div
-            layoutId="navigation-underline"
-            className={styles.NavUnderline}
-            animate
-          />
-          {props.name}
-        </a>
-      </Link>
+      <motion.div
+        initial="hidden"
+        animate="visible"
+        variants={variants}
+        custom={props.id}
+      >
+        <Link key={props.name} href={props.href} scroll={false}>
+          <a className={`${styles.Link} ${styles.IsActive}`}>
+            <motion.div
+              layoutId="navigation-underline"
+              className={styles.NavUnderline}
+              animate
+            />
+            {props.name}
+          </a>
+        </Link>
+      </motion.div>
     );
   }
   return (
-    <Link key={props.name} href={props.href} scroll={false}>
-      <a className={styles.Link}>{props.name}</a>
-    </Link>
+    <motion.div
+      initial="hidden"
+      animate="visible"
+      variants={variants}
+      custom={props.id}
+    >
+      <Link key={props.name} href={props.href} scroll={false}>
+        <a className={styles.Link}>{props.name}</a>
+      </Link>
+    </motion.div>
   );
 }
 
@@ -45,8 +75,9 @@ const Navigation = () => {
   return (
     <AnimateSharedLayout>
       <nav className={styles.Navigation}>
-        {links.map(({ name, href }) => (
+        {links.map(({ id, name, href }) => (
           <NavItem
+            id={id}
             key={name}
             href={href}
             name={name}

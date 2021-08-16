6467cd5e138d51b636939482bfe1eeb149259a18
Maximilian Hagerf <maximilian.hagerf@gmail.com>
2021-08-16 03:19:39 -0400

    no animation on Content component, redundant, might remove later

--- components/Content.js
+++ components/Content.js
@@ -1,25 +1,8 @@
 import { motion } from "framer-motion";
 import styles from "../styles/modules/Content.module.css";
 
-const variants = {
-  hidden: { opacity: 0, x: -200, y: 0 },
-  enter: { opacity: 1, x: 0, y: 0 },
-  exit: { opacity: 0, x: 300, y: 0 },
-};
-
 const Content = ({ children }) => {
-  return (
-    <motion.main
-      variants={variants} // Pass the variant object into Framer Motion
-      initial="hidden" // Set the initial state to variants.hidden
-      animate="enter" // Animated state to variants.enter
-      exit="exit" // Exit state (used later) to variants.exit
-      transition={{ type: "linear" }} // Set the transition to linear
-      className={styles.Main}
-    >
-      {children}
-    </motion.main>
-  );
+  return <main className={styles.Main}>{children}</main>;
 };
 
 export default Content;

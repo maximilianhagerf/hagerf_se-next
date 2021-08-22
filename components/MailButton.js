d4f2504a2e78ebbb3b86618f9ef7ddfdd54dabbb
Maximilian Hagerf <maximilian.hagerf@gmail.com>
2021-08-22 14:16:55 -0400

    responsive mailbutton

--- components/MailButton.js
+++ components/MailButton.js
@@ -14,13 +14,15 @@ const confirmVariants = {
 
 const containerVariants = {
   hidden: {
-    x: 130,
+    display: "flex",
+    x: "calc(100% + 3rem)",
     transition: {
       delay: 0.8,
       duration: 0.5,
     },
   },
   visible: {
+    display: "flex",
     x: 0,
     transition: {
       delay: 1.8,
@@ -50,15 +52,12 @@ const MailButton = () => {
     } else {
       mainControls.start("visible");
     }
-  }, [mainControls, router]);
-
-  useEffect(() => {
     if (isCopied) {
       controls.start("visible");
     } else {
       controls.start("hidden");
     }
-  }, [controls, isCopied]);
+  }, [mainControls, router, controls, isCopied]);
 
   const onClick = () => {
     copyTextToClipboard("maximilian.hagerf@gmail.com")
@@ -79,58 +78,59 @@ const MailButton = () => {
       animate={mainControls}
       exit="hidden"
       variants={containerVariants}
-      className={styles.MailButton}
-      onClick={onClick}
+      className={styles.MailButtonContainer}
     >
-      <motion.span
-        initial="hidden"
-        animate={controls}
-        variants={confirmVariants}
-        className={styles.Confirm}
-      >
-        Copied!
-      </motion.span>
-      <span className={styles.Text}>
-        maximilian.hagerf@gmail.com
-        <i className={styles.TextInside}>CLICK TO COPY</i>
-      </span>
-      <span className={styles.IconContainer}>
-        <i className={styles.Icon}>
-          <svg
-            className={styles.Svg}
-            xmlns="http://www.w3.org/2000/svg"
-            width="36.295"
-            height="19.281"
-            viewBox="0 0 50 20"
-          >
-            <g
-              id="Vector_Smart_Object"
-              data-name="Vector Smart Object"
-              transform="translate(6 -0.86) scale(1 )"
+      <div className={styles.MailButton} onClick={onClick}>
+        <motion.span
+          initial="hidden"
+          animate={controls}
+          variants={confirmVariants}
+          className={styles.Confirm}
+        >
+          Copied!
+        </motion.span>
+        <span className={styles.Text}>
+          maximilian.hagerf@gmail.com
+          <i className={styles.TextInside}>CLICK TO COPY</i>
+        </span>
+        <span className={styles.IconContainer}>
+          <i className={styles.Icon}>
+            <svg
+              className={styles.Svg}
+              xmlns="http://www.w3.org/2000/svg"
+              width="36.295"
+              height="19.281"
+              viewBox="0 0 50 20"
             >
-              <path
-                id="Path_3"
-                data-name="Path 3"
-                d="M3.439,2.532,1.6,1.61V19.39H36.4V1.61L19,10.349Z"
-                fill="none"
-                stroke="#f5df4d"
-                strokeLinejoin="round"
-                strokeWidth="2"
-              />
-              <line
-                id="Line_1"
-                data-name="Line 1"
-                x2="34.796"
-                transform="translate(1.602 1.61)"
-                fill="none"
-                stroke="#f5df4d"
-                strokeLinejoin="round"
-                strokeWidth="2"
-              />
-            </g>
-          </svg>
-        </i>
-      </span>
+              <g
+                id="Vector_Smart_Object"
+                data-name="Vector Smart Object"
+                transform="translate(6 -0.86) scale(1 )"
+              >
+                <path
+                  id="Path_3"
+                  data-name="Path 3"
+                  d="M3.439,2.532,1.6,1.61V19.39H36.4V1.61L19,10.349Z"
+                  fill="none"
+                  stroke="#f5df4d"
+                  strokeLinejoin="round"
+                  strokeWidth="2"
+                />
+                <line
+                  id="Line_1"
+                  data-name="Line 1"
+                  x2="34.796"
+                  transform="translate(1.602 1.61)"
+                  fill="none"
+                  stroke="#f5df4d"
+                  strokeLinejoin="round"
+                  strokeWidth="2"
+                />
+              </g>
+            </svg>
+          </i>
+        </span>
+      </div>
     </motion.div>
   );
 };

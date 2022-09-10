fc7b8ced2eee1e340f06cd1f65ae23fb3561fc34
Maximilian Hagerf <maximilian.hagerf@gmail.com>
2022-09-10 11:10:58 -0400

    fix:develop

--- next.config.js
+++ next.config.js
@@ -1,6 +1,9 @@
+const isProd = process.env.NODE_ENV === "production";
+
 module.exports = {
   basePath: process.env.NEXT_PUBLIC_BASE_PATH,
-  assetPrefix: process.env.NEXT_PUBLIC_BASE_PATH,
+  // assetPrefix: process.env.NEXT_PUBLIC_BASE_PATH,
+  assetPrefix: isProd ? "https://hagerf.se" : process.env.NEXT_PUBLIC_BASE_PATH,
   reactStrictMode: true,
   trailingSlash: true,
   swcMinify: true,

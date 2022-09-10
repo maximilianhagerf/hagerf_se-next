df20ad350cb0483082c18431eceaf1b35da7cb5c
Maximilian Hagerf <maximilian.hagerf@gmail.com>
2022-09-10 01:25:34 -0400

    fix: ffs....

--- next.config.js
+++ next.config.js
@@ -1,8 +1,8 @@
 module.exports = {
-  // basePath: process.env.NEXT_PUBLIC_BASE_PATH,
-  // assetPrefix: process.env.NEXT_PUBLIC_BASE_PATH,
-  basePath: "",
-  assetPrefix: "./",
+  basePath: process.env.NEXT_PUBLIC_BASE_PATH,
+  assetPrefix: process.env.NEXT_PUBLIC_BASE_PATH,
+  // basePath: "",
+  // assetPrefix: "./",
   reactStrictMode: true,
   trailingSlash: true,
   swcMinify: true,

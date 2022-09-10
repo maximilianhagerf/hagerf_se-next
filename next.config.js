7da0979576eaf769ecd5ea9fe72f0f304f9a0be9
Maximilian Hagerf <maximilian.hagerf@gmail.com>
2022-09-10 02:22:13 -0400

    fix: deploy

--- next.config.js
+++ next.config.js
@@ -1,6 +1,7 @@
 module.exports = {
   basePath: process.env.NEXT_PUBLIC_BASE_PATH,
-  assetPrefix: process.env.NEXT_PUBLIC_BASE_PATH,
+  // assetPrefix: process.env.NEXT_PUBLIC_BASE_PATH,
+  assetPrefix: "./",
   reactStrictMode: true,
   trailingSlash: true,
   swcMinify: true,

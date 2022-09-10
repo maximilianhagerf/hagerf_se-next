59e5721676ced2c8a07557f4d352838ac403edf1
Maximilian Hagerf <maximilian.hagerf@gmail.com>
2022-09-10 01:39:03 -0400

    fix: ffs

--- next.config.js
+++ next.config.js
@@ -1,17 +1,15 @@
 module.exports = {
   basePath: process.env.NEXT_PUBLIC_BASE_PATH,
   assetPrefix: process.env.NEXT_PUBLIC_BASE_PATH,
-  // basePath: "",
-  // assetPrefix: "./",
   reactStrictMode: true,
   trailingSlash: true,
   swcMinify: true,
-  // images: {
-  //   loader: "imgix",
-  //   path: "/",
-  // },
   images: {
-    loader: "akamai",
-    path: "",
+    loader: "imgix",
+    path: "/",
   },
+  // images: {
+  //   loader: "akamai",
+  //   path: "",
+  // },
 };

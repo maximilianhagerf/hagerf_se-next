a8595ecc54471776b661702957911b9cf0c56af2
Maximilian Hagerf <maximilian.hagerf@gmail.com>
2022-09-11 10:21:57 -0400

    fix: next config from master

--- next.config.js
+++ next.config.js
@@ -1,11 +1,13 @@
+const isProd = process.env.NODE_ENV === "production";
+
 module.exports = {
   basePath: process.env.NEXT_PUBLIC_BASE_PATH,
   assetPrefix: process.env.NEXT_PUBLIC_BASE_PATH,
   reactStrictMode: true,
   trailingSlash: true,
   swcMinify: true,
-  // images: {
-  //   loader: "imgix",
-  //   path: "/",
-  // },
+  images: {
+    loader: "akamai",
+    path: isProd ? "https://hagerf.se" : "http://localhost:3000",
+  },
 };
